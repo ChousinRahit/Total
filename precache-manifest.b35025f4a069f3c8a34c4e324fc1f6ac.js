eaffcc1a17bad64a0e312563b4279948aa0d8254
ChousinRahit <2428kcr@gmail.com>
2019-11-25 07:45:38 +0530

    Updates

---
+++ precache-manifest.b35025f4a069f3c8a34c4e324fc1f6ac.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f16f2584cf6c8c86365c43370d31b7f",
+    "url": "/Total/index.html"
+  },
+  {
+    "revision": "3bad7abc66737939e1c0",
+    "url": "/Total/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "728b179e7df8cf69d6be",
+    "url": "/Total/static/css/main.15173ee3.chunk.css"
+  },
+  {
+    "revision": "3bad7abc66737939e1c0",
+    "url": "/Total/static/js/2.12c4e699.chunk.js"
+  },
+  {
+    "revision": "728b179e7df8cf69d6be",
+    "url": "/Total/static/js/main.b4b45973.chunk.js"
+  },
+  {
+    "revision": "a45cd8fe888e9e83218d",
+    "url": "/Total/static/js/runtime-main.455f20ac.js"
+  },
+  {
+    "revision": "86e3b4918367db80ec83a155924e065f",
+    "url": "/Total/static/media/img1.86e3b491.png"
+  },
+  {
+    "revision": "7d2b44f861ec20157ca724aab3a61fdf",
+    "url": "/Total/static/media/ivan-torres-MQUqbmszGGM-unsplash.7d2b44f8.jpg"
+  },
+  {
+    "revision": "76344adc9679041868afd66d041341bb",
+    "url": "/Total/static/media/list1.76344adc.png"
+  },
+  {
+    "revision": "7d4c21df2a975b82e5ba1b8ba9cfb317",
+    "url": "/Total/static/media/list2.7d4c21df.jpg"
+  },
+  {
+    "revision": "820b9d247caa3dcea1ade10602ff718b",
+    "url": "/Total/static/media/list3.820b9d24.jpg"
+  },
+  {
+    "revision": "dbdba0cf0bf0557d53d68c5e02bffda1",
+    "url": "/Total/static/media/list4.dbdba0cf.jpg"
+  }
+]);
