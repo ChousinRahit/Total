f9eac737ddc7a4094305cf9e1120f4ad82c078bd
ChousinRahit <2428kcr@gmail.com>
2019-11-25 10:34:49 +0530

    links changed

--- src/components/LandingView/index.jsx
+++ src/components/LandingView/index.jsx
@@ -1,20 +1,6 @@
 import React from "react";
-import { Container, Row, Col, Button } from "reactstrap";
-import { FiChevronsDown, FiPocket } from "react-icons/fi";
-// import {
-//   ,
-//   Collapse,
-//   Navbar,
-//   NavbarToggler,
-//   NavbarBrand,
-//   Nav,
-//   NavItem,
-//   NavLink,
-//   UncontrolledDropdown,
-//   DropdownToggle,
-//   DropdownMenu,
-//   DropdownItem
-// } from "reactstrap";
+import {FiPocket } from "react-icons/fi";
+
 
 import TextView from "./TextView";
 import classes from "./style.module.css";
